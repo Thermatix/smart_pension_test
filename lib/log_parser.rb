5546ae53618af35c66de55930d6e15f692749baa
Martin Becker <mbeckerwork@gmail.com>
2019-10-15 15:14:45 +0100

    Allow for simple arg parsing

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Log_Parser
-  attr_reader :log_location
-  def initialize(log_location)
+  attr_reader :log_location, :args
+  def initialize(log_location, args)
     @log_location = log_location
+    @args = args
   end
 
   def log_file_exists?
